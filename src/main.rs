2147156a8ac71fb417fd12125656dc4b40c45e15
Sian Cao <yinshuiboy@gmail.com>
2017-02-08 18:21:51 +0800

    Reformat code and add options

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 extern crate xcb;
+extern crate getopts;
+
 use std::io::Write;
 use std::fmt::*;
 
@@ -53,7 +55,34 @@ enum XcbRequest<'a> {
     GP(xcb::GetPropertyCookie<'a>),
 }
 
+fn usage(program: &String, opts: &getopts::Options)
+{
+    let brief = format!("Usage: {} [vcmof]", program);
+    println!("{}", opts.usage(&brief));
+}
+
 pub fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let program = args[0].clone();
+
+    let mut opts = getopts::Options::new();
+    opts.optflag("v", "only-mapped", "show only mapped windows");
+	opts.optflag("c", "colored", "output info with color");
+	opts.optflag("m", "monitor", "run in monitor mode");
+	//opts.optflag("f", "filter", "filter rule");
+	opts.optflag("o", "omit-hidden", "omit hidden windows");
+	opts.optflag("h", "help", "show this help");
+
+    let args = match opts.parse(&args) {
+        Ok(m) => m,
+        Err(_) => { usage(&program, &opts); return; },
+    };
+
+    if args.opt_present("h") {
+        usage(&program, &opts);
+        return;
+    }
+
     let (c, _) = xcb::Connection::connect(None).unwrap();
     let screen = c.get_setup().roots().next().unwrap();
 
@@ -77,6 +106,14 @@ pub fn main() {
 
     let mut windows = Vec::with_capacity(res.children_len() as usize);
 
+    macro_rules! apply_reply {
+        ($win:ident $cookie:ident $reply:ident $e:expr) => (
+            match $cookie.get_reply() {
+                Ok($reply) => $e,
+                Err(_) => $win.valid = false,
+            })
+    }
+
     let window_ids = res.children();
     for (i, query) in qs.into_iter().enumerate() {
         let idx = i / 4;
@@ -90,49 +127,36 @@ pub fn main() {
             });
         }
 
-        match query {
-            XcbRequest::GWA(cookie) => {
-                if let Some(win) = windows.last_mut() {
-                    match cookie.get_reply() {
-                        Ok(reply) => {
-                            win.attrs.override_redirect = reply.override_redirect();
-                            win.attrs.map_state = match reply.map_state() {
-                                0 => MapState::Unmapped,
-                                2 => MapState::Viewable,
-                                _ => MapState::Unviewable,
-                            };
-                        },
-                        Err(_) => win.valid = false,
-                    }
-                }
-            },
-            XcbRequest::GE(cookie) => {
-                if let Some(win) = windows.last_mut() {
-                    match cookie.get_reply() {
-                        Ok(reply) => {
-                            win.geom = Geometry {
-                                x: reply.x() as i32, 
-                                y: reply.y() as i32,
-                                width: reply.width() as i32,
-                                height: reply.height() as i32,
-                            };
-                        },
-                        Err(_) => win.valid = false,
-                    }
-                }
-            },
-            XcbRequest::GP(cookie) => {
-                if let Some(win) = windows.last_mut() {
-                    match cookie.get_reply() {
-                        Ok(reply) => {
-                            if reply.value_len() > 0 && win.name.len() == 0 {
-                                win.name = String::from_utf8(reply.value::<u8>().to_vec()).unwrap_or("".to_string());
-                            }
-                        },
-                        Err(_) => win.valid = false,
-                    }
-                }
-            },
+        if let Some(win) = windows.last_mut() {
+            match query {
+                XcbRequest::GWA(cookie) => {
+                    apply_reply!(win cookie reply {
+                        win.attrs.override_redirect = reply.override_redirect();
+                        win.attrs.map_state = match reply.map_state() {
+                            0 => MapState::Unmapped,
+                            2 => MapState::Viewable,
+                            _ => MapState::Unviewable,
+                        };
+                    })
+                },
+                XcbRequest::GE(cookie) => {
+                    apply_reply!(win cookie reply {
+                        win.geom = Geometry {
+                            x: reply.x() as i32, 
+                            y: reply.y() as i32,
+                            width: reply.width() as i32,
+                            height: reply.height() as i32,
+                        };
+                    })
+                },
+                XcbRequest::GP(cookie) => {
+                    apply_reply!(win cookie reply {
+                        if reply.value_len() > 0 && win.name.len() == 0 {
+                            win.name = String::from_utf8(reply.value::<u8>().to_vec()).unwrap_or("".to_string());
+                        }
+                    })
+                },
+            }
         }
 
     }
