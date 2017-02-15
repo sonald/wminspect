5aeffe0439fee133d7d1b834cc65928a4bbace22
Sian Cao <yinshuiboy@gmail.com>
2017-02-15 20:18:24 +0800

    Implement monitor skeleton

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate colored;
 use colored::*;
 use std::fmt::*;
 use std::env;
+use std::time;
+use std::thread;
+use xcb::xproto;
 
 #[derive(Debug, Clone, Copy)]
 struct Geometry<T> where T: Copy {
@@ -29,15 +32,15 @@ impl PartialEq for MapState {
 
 impl Eq for MapState { }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Attributes {
     override_redirect: bool,
     map_state: MapState,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Window {
-    id: xcb::ffi::xcb_window_t,
+    id: xcb::Window,
     name: String,
     attrs: Attributes,
     geom: Geometry<i32>,
@@ -73,6 +76,69 @@ fn usage(program: &String, opts: &getopts::Options)
 {
     let brief = format!("Usage: {} [vcmof]", program);
     println!("{}", opts.usage(&brief));
+}
+
+fn query_window(c: &xcb::Connection, id: xcb::Window) -> Window {
+    let net_wm_name_atom = xcb::intern_atom(&c, false, "_NET_WM_NAME").get_reply().unwrap();
+    let utf8_string_atom = xcb::intern_atom(&c, false, "UTF8_STRING").get_reply().unwrap();
+
+    let mut qs: Vec<XcbRequest> = Vec::new();
+    qs.push(XcbRequest::GWA(xcb::get_window_attributes(&c, id)));
+    qs.push(XcbRequest::GE(xcb::get_geometry(&c, id)));
+    qs.push(XcbRequest::GP(xcb::get_property(&c, false, id, net_wm_name_atom.atom(),
+        utf8_string_atom.atom(), 0, std::u32::MAX)));
+    qs.push(XcbRequest::GP(xcb::get_property(&c, false, id, xcb::xproto::ATOM_WM_NAME, 
+                                             xcb::xproto::ATOM_STRING, 0, std::u32::MAX)));
+
+    macro_rules! apply_reply {
+        ($win:ident $cookie:ident $reply:ident $e:expr) => (
+            match $cookie.get_reply() {
+                Ok($reply) => $e,
+                Err(_) => $win.valid = false,
+            })
+    }
+
+    let mut win = Window {
+        id: id,
+        name: "".to_string(),
+        attrs: Attributes{override_redirect: false, map_state: MapState::Unmapped},
+        geom: Geometry{x:0,y:0,width:0,height:0},
+        valid: true,
+    };
+
+    for query in qs {
+        match query {
+            XcbRequest::GWA(cookie) => {
+                apply_reply!(win cookie reply {
+                    win.attrs.override_redirect = reply.override_redirect();
+                    win.attrs.map_state = match reply.map_state() {
+                        0 => MapState::Unmapped,
+                        2 => MapState::Viewable,
+                        _ => MapState::Unviewable,
+                    };
+                })
+            },
+            XcbRequest::GE(cookie) => {
+                apply_reply!(win cookie reply {
+                    win.geom = Geometry {
+                        x: reply.x() as i32, 
+                        y: reply.y() as i32,
+                        width: reply.width() as i32,
+                        height: reply.height() as i32,
+                    };
+                })
+            },
+            XcbRequest::GP(cookie) => {
+                apply_reply!(win cookie reply {
+                    if reply.value_len() > 0 && win.name.len() == 0 {
+                        win.name = String::from_utf8(reply.value::<u8>().to_vec()).unwrap_or("".to_string());
+                    }
+                })
+            },
+        }
+    }
+
+    return win;
 }
 
 fn query_windows(c: &xcb::Connection, res: &xcb::QueryTreeReply) -> Vec<Window> {
@@ -148,6 +214,48 @@ fn query_windows(c: &xcb::Connection, res: &xcb::QueryTreeReply) -> Vec<Window> 
     return windows;
 }
 
+macro_rules! do_filter {
+    ($windows:ident, $op:ident, $rule:expr) => (
+        $windows = $windows.into_iter(). $op ( $rule ) .collect::<Vec<_>>();
+        )
+}
+
+fn collect_windows(c: &xcb::Connection, args: &getopts::Matches) -> Vec<Window> {
+    let screen = c.get_setup().roots().next().unwrap();
+    let res = match xcb::query_tree(&c, screen.root()).get_reply() {
+        Ok(result) => result,
+        Err(_) => return Vec::new(),
+    };
+
+    let mut target_windows = query_windows(&c, &res);
+
+    if args.opt_present("o") || args.opt_present("v") {
+        let has_guard_window = target_windows.as_slice().iter()
+            .any(|ref w| w.name.contains("guard window") && w.attrs.override_redirect);
+
+        do_filter!(target_windows, skip_while, |ref w| {
+            if has_guard_window {
+                !w.name.contains("guard window") || !w.attrs.override_redirect
+            } else {
+                false
+            }
+        });
+
+        if args.opt_present("v") {
+            do_filter!(target_windows, filter, |w: &Window| { w.attrs.map_state == MapState::Viewable });
+        }
+
+        if args.opt_present("o") {
+            do_filter!(target_windows, filter, |ref w| {
+                w.geom.x < screen.width_in_pixels() as i32 && w.geom.y < screen.height_in_pixels() as i32 &&
+                    w.geom.width + w.geom.x > 0 && w.geom.height + w.geom.y > 0
+            });
+        }
+    }
+
+    return target_windows;
+}
+
 pub fn main() {
     let args = std::env::args().collect::<Vec<String>>();
     let program = args[0].clone();
@@ -173,61 +281,75 @@ pub fn main() {
     let (c, _) = xcb::Connection::connect(None).unwrap();
     let screen = c.get_setup().roots().next().unwrap();
 
-    let res = match xcb::query_tree(&c, screen.root()).get_reply() {
-        Ok(result) => result,
-        Err(_) => return,
-    };
-
-    let windows = query_windows(&c, &res);
-
-
-    let mut target_windows = windows;
-    
-    macro_rules! do_filter {
-        ($windows:ident, $op:ident, $rule:expr) => (
-            $windows = $windows.into_iter(). $op ( $rule ) .collect::<Vec<_>>();
-        )
+    if args.opt_present("m") {
+        monitor(&c, &screen, &args);
+    } else {
+        let windows = collect_windows(&c, &args);
+        dump_windows(&windows, &args);
     }
+}
 
-    if args.opt_present("o") || args.opt_present("v") {
-        let mut has_guard_window = false;
-        for w in &target_windows {
-            if w.name.contains("guard window") && w.attrs.override_redirect {
-                has_guard_window = true;
-                break;
-            }
+
+fn monitor(c: &xcb::Connection, screen: &xcb::Screen, args: &getopts::Matches) {
+    let ev_mask: u32 = xproto::EVENT_MASK_SUBSTRUCTURE_NOTIFY;
+    xcb::xproto::change_window_attributes(&c, screen.root(), 
+                      &[(xcb::xproto::CW_EVENT_MASK, ev_mask)]);
+    c.flush();
+
+    let mut windows = collect_windows(c, args);
+    dump_windows(&windows, args);
+
+    loop {
+        if let Some(ev) = c.poll_for_event() {
+            match ev.response_type() & !0x80 {
+                xcb::xproto::CREATE_NOTIFY => {
+                    let cne = xcb::cast_event::<xcb::CreateNotifyEvent>(&ev);
+                    if cne.parent() != screen.root() {
+                        break;
+                    }
+                    println!("create 0x{:x}, parent 0x{:x}", cne.window(), cne.parent());
+
+                    windows = collect_windows(c, args);
+                    dump_windows(&windows, args);
+                },
+                xcb::xproto::DESTROY_NOTIFY => {
+                    let dne = xcb::cast_event::<xcb::DestroyNotifyEvent>(&ev);
+
+                    println!("destroy 0x{:x}", dne.window());
+                    do_filter!(windows, filter, |ref w| w.id != dne.window());
+                    dump_windows(&windows, args);
+                },
+                xcb::xproto::REPARENT_NOTIFY => {
+                    let rne = xcb::cast_event::<xcb::ReparentNotifyEvent>(&ev);
+                    println!("reparent 0x{:x} to 0x{:x}", rne.window(), rne.parent());
+
+                    if rne.parent() != screen.root() {
+                        do_filter!(windows, filter, |ref w| w.id != rne.window());
+                    }
+                    dump_windows(&windows, args);
+                },
+
+                xproto::CONFIGURE_NOTIFY => {
+                    let cne = xcb::cast_event::<xcb::ConfigureNotifyEvent>(&ev);
+
+                    //TODO: squeeze consective configurenotify for the same window
+                    let do_recollect = windows.as_slice().iter().any(|ref w| w.id == cne.window());
+                    if do_recollect {
+                        println!("configure 0x{:x} above: 0x{:x}", cne.window(), cne.above_sibling());
+                        windows = collect_windows(c, args);
+                        dump_windows(&windows, args);
+                    }
+                },
+
+                _ => {
+                },
+            } 
         };
 
-        do_filter!(target_windows, skip_while, |ref w| {
-            if has_guard_window {
-                !w.name.contains("guard window") || !w.attrs.override_redirect
-            } else {
-                false
-            }
-        });
-
-        if args.opt_present("v") {
-            do_filter!(target_windows, filter, |w: &Window| { w.attrs.map_state == MapState::Viewable });
-        }
-
-        if args.opt_present("o") {
-            do_filter!(target_windows, filter, |ref w| {
-                w.geom.x < screen.width_in_pixels() as i32 && w.geom.y < screen.height_in_pixels() as i32 &&
-                    w.geom.width + w.geom.x > 0 && w.geom.height + w.geom.y > 0
-            });
-        }
-    }
-
-    if args.opt_present("m") {
-        monitor(&target_windows);
-    } else {
-        dump_windows(&target_windows, args.opt_present("c"));
+        thread::sleep(time::Duration::from_millis(50));
     }
 }
 
-
-fn monitor(windows: &Vec<Window>) {
-}
 fn win2str(w: &Window, colored: bool) -> String {
     let geom_str = format!("{}x{}+{}+{}", w.geom.width, w.geom.height,
                            w.geom.x, w.geom.y).red();
@@ -250,7 +372,8 @@ fn win2str(w: &Window, colored: bool) -> String {
     }
 }
 
-fn dump_windows(windows: &Vec<Window>, colored: bool) {
+fn dump_windows(windows: &Vec<Window>, args: &getopts::Matches) {
+    let colored = args.opt_present("c");
     for (i, w) in windows.into_iter().enumerate() {
         println!("{}: {}", i, win2str(w, colored));
     }
